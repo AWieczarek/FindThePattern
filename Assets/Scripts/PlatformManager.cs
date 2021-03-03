d2c08f7065a68d49c77a559ec41f35d5205c4521
AWieczarek <adamwie12@gmail.com>
2021-03-03 15:03:05 +0100

    Add disabling platform

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -6,17 +6,32 @@ public class PlatformManager : MonoBehaviour
 {
     public GameObject platformPrefab;
     public Transform player;
-    public int fCount = 10;
-    public int wCount = 2;
+    public int fCount = 100;
+    public int wCount = 20;
     public float offsetChange = 1.5f;
+    private GameObject[,] platforms;
+
     void Start()
     {
+        platforms = new GameObject[fCount, wCount*2];
         for (int i = 0; i < fCount; i++)
         {
             for (int j = -wCount; j < wCount; j++)
             {
-                Instantiate(platformPrefab, new Vector3(j * offsetChange, 0, i * offsetChange), Quaternion.identity);
+                platforms[i,j+wCount] = Instantiate(platformPrefab, new Vector3(j * offsetChange, 0, i * offsetChange), Quaternion.identity);
             }
         }
+
+        for (int j = -wCount; j < wCount; j++)
+        {
+            int x = EmptyPosition();
+            platforms[x, j + wCount].GetComponent<Renderer>().material.color = Color.green;
+            platforms[x, j + wCount].GetComponent<BoxCollider>().isTrigger = true;
+        }
     }
+
+    public int EmptyPosition()
+	{
+        return Random.Range(5, 19);
+	}
 }
