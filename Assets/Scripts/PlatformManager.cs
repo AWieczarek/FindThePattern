3d6767cdf863440f982a164e31de72ebaa5e1aef
AWieczarek <adamwie12@gmail.com>
2021-03-03 19:36:23 +0100

    Add dificulty level

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -10,7 +10,9 @@ public class PlatformManager : MonoBehaviour
     public int wCount = 20;
     public float offsetChange = 1.5f;
     private GameObject[,] platforms;
-
+    int step = 0;
+    int level = 2;
+    int pom = 0;
     void Start()
     {
         platforms = new GameObject[fCount, wCount*2];
@@ -21,17 +23,49 @@ public class PlatformManager : MonoBehaviour
                 platforms[i,j+wCount] = Instantiate(platformPrefab, new Vector3(j * offsetChange, 0, i * offsetChange), Quaternion.identity);
             }
         }
+    }
 
+	private void Update()
+	{
+		if(player.position.z > pom)
+		{
+			for (int i = 0; i < level; i++)
+			{
+                Holes();
+            }
+
+            if (step == 80)
+            {
+                step = 0;
+                level++;
+            }
+            else if (step == 40)
+            {
+                step += 20;
+                pom += 20;
+                level++;
+            }
+            else
+            {
+                step += 20;
+                pom += 20;
+            }
+        }
+	}
+
+	public int EmptyPosition()
+	{
+        return Random.Range(2 + step, 20 + step);
+	}
+
+    void Holes()
+	{
         for (int j = -wCount; j < wCount; j++)
         {
             int x = EmptyPosition();
+            Debug.Log(x);
             platforms[x, j + wCount].GetComponent<Renderer>().material.color = Color.green;
             platforms[x, j + wCount].GetComponent<BoxCollider>().isTrigger = true;
         }
     }
-
-    public int EmptyPosition()
-	{
-        return Random.Range(5, 19);
-	}
 }
